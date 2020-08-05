d65b7147ce77023cf2d3defe8f657f3a8d12cf38
akarX23 <ritikagrawal1292000@gmail.com>
2020-08-05 23:40:49 +0530

    pls

--- server/server.js
+++ server/server.js
@@ -207,7 +207,8 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
 });
 
-const port = process.env.port || 5000;
-app.listen(port, () => {
+const port = process.env.PORT || 5000;
+const host = "0.0.0.0";
+app.listen(port, host, () => {
   console.log("Server Running");
 });
